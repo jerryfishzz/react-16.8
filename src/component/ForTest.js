6e07a982e8b815c7ffc163fc45f0dd3374366f62
jerryfishzz <jerryfish@163.com>
2019-08-26 22:44:51 +1200

    draft js
    chore: Test

--- src/component/ForTest.js
+++ src/component/ForTest.js
@@ -1,12 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 const useStyles = makeStyles({
   root: {
@@ -18,37 +13,115 @@ const useStyles = makeStyles({
   input: {
     marginLeft: 8,
     flex: 1,
+    backgroundColor: 'white',
+    padding: 6
   },
-  iconButton: {
+  draftContent: {
+    flex: 1
+  },
+  editor: {
+    border: '1px solid',
     padding: 10,
   },
-  divider: {
-    width: 1,
-    height: 28,
-    margin: 4,
+  btn: {
+    background: '#999',
+    color: '#fff',
+    border: 'none',
+    padding: '.5em',
+    cursor: 'pointer',
+    marginBottom: '1em',
+    marginRight: '1em',
+    borderRadius: '.2em',
+    '&:hover': {
+      background: '#888',
+    }
   },
+  
 });
+
+function myBlockStyleFn(contentBlock) {
+  const type = contentBlock.getType();
+  if (type === 'code-block') {
+    return 'superFancyBlockquote';
+  }
+}
 
 export default function ForTest() {
   const classes = useStyles();
+  const [editorState, setEditorState] = useState(EditorState.createEmpty())
+
+  const onChange = editorState => {
+    setEditorState(editorState)
+  }
+
+  const handleKeyCommand = command => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if (newState) {
+      onChange(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  const onUnderlineClick = () => {
+    onChange(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
+  }
+
+  const onToggleCode = () => {
+    onChange(RichUtils.toggleCode(editorState));
+  }
 
   return (
     <Paper className={classes.root}>
-      <IconButton className={classes.iconButton} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        className={classes.input}
-        placeholder="Search Google Maps"
-        inputProps={{ 'aria-label': 'search google maps' }}
-      />
-      <IconButton className={classes.iconButton} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider className={classes.divider} />
-      <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
+      <div className={classes.draftContent}>
+        <div className={classes.editor}>
+          <button 
+            onClick={onUnderlineClick}
+            className={classes.btn}
+          >
+            Underline
+          </button>
+          <button 
+            onClick={onToggleCode}
+            className={classes.btn}
+          >
+            Code Block
+          </button>
+          <Editor
+            editorState={editorState}
+            onChange={onChange}
+            handleKeyCommand={handleKeyCommand}
+            blockStyleFn={myBlockStyleFn}
+          />
+        </div>
+      </div>
+      <style type="text/css">
+        {`
+        .superFancyBlockquote {
+          color: red;
+          font-family: 'Hoefler Text', Georgia, serif;
+          font-style: italic;
+          text-align: center;
+        }
+
+        pre {
+          border: 1px solid #ccc;
+          background: #f0f0f0;
+          border-radius: .2em;
+          padding: .5em;
+          margin: 0;
+        }
+
+
+        pre > pre {
+          background: none;
+          border: none;
+          padding: 0;
+        }
+        `}
+      </style>
     </Paper>
   );
 }
