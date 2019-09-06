d6e3bd9b54d1091c9adb2eddadea3eec474d6b29
jerryfishzz <jerryfish@163.com>
2019-09-06 20:33:01 +1200

    netlify initial error
    chore: Test

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -105,6 +105,7 @@ export function createAnswerContainer(id) {
 }
 
 export function handleErrors(response) {
+  console.log(response)
   if (!response.ok) {
       throw Error(response.statusText);
   }
