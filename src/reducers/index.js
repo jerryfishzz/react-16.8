fbe8478d2c49bdeb2f1ac6928bbcfa134af6eefc
jerryfishzz <jerryfish@163.com>
2019-09-23 00:18:30 +1200

    clean up
    chore: Clean up code in reducer and actions folders

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from "redux";
 import test from "./test/test";
 import tags from "./tags";
-import postType from "./postType";
 import questionList from "./questionList";
 
 
 export default combineReducers({
   test,
   tags,
-  postType,
   questionList
 })
