75dffcfeae0348786b4a39fd69b3e524c9097546
jerryfishzz <jerryfish@163.com>
2019-12-30 21:29:34 +1300

    install date-time
    feat: Install date-time from npm for getting the customized current time format much easier
    
    Probably not useful any more due to the wrong idea on checking data before modifying

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import * as R from 'ramda'
+import dateTime from 'date-time'
 
 import { 
   _getQuestions, 
@@ -87,6 +88,20 @@ export async function getQuestionsFromWordPress(postType) {
     const concatFirstPageDataWithOtherPages = R.reduce(concatTwoPagesData, firstPageData)
     const getAllPagesData = R.pipe(selectRestPagesData, concatFirstPageDataWithOtherPages)
     const allPagesData = getAllPagesData(restPagesResponses)
+
+
+
+
+console.log(allPagesData)
+    console.log(allPagesData[0].date)
+    console.log(allPagesData[0].date_gmt)
+    console.log(allPagesData[0].modified)
+    console.log(typeof allPagesData[0].modified_gmt)
+
+    console.log(allPagesData[0].modified > allPagesData[0].date ? true : false)
+    console.log(new Date())
+    console.log(new Date().getTime())
+    console.log(dateTime({local: false}).replace(' ', 'T'))
 
     return allPagesData
   } catch (err) {
