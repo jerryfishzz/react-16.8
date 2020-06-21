53a2667526ec6cbd5cb7d17f30ccff597d6c6aff
jerryfishzz <jerryfish@163.com>
2020-06-21 21:40:07 +1200

    draft js code
    feat: Install draft js code and upgrade draft js plugin and implement draft js code
    
    - Install draft js code and implement it
    
    - Add a style for code block in the editor for tests later

--- src/component/tests/Form/DraftEditor.js
+++ src/component/tests/Form/DraftEditor.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { RichUtils } from 'draft-js';
-import clearFormatting from 'draft-js-clear-formatting'
+import { RichUtils, getDefaultKeyBinding } from 'draft-js';
 import Editor from 'draft-js-plugins-editor'
 import { Tooltip, Grid } from '@material-ui/core';
+import CodeUtils from 'draft-js-code'
 
 const useStyles = makeStyles(theme => ({
   editor: {
@@ -30,13 +30,26 @@ const useStyles = makeStyles(theme => ({
 
 export default function DraftEditor(props) {
   const classes = useStyles();
+  const { contents } = props
 
   const onChange = editorState => {
     props.handleDraftChange(editorState)
   }
 
-  const handleKeyCommand = command => {
-    const newState = RichUtils.handleKeyCommand(props.contents, command);
+  // This method looks only deal with those commands
+  // which can modify the editor content.
+  // Keys like left, right, up, and down won't go to this method.
+  const handleKeyCommand = (command, editorState) => {
+    let newState;
+
+    // If in code block, use draft js code to create the new state
+    if (CodeUtils.hasSelectionInBlock(editorState)) {
+      newState = CodeUtils.handleKeyCommand(editorState, command);
+    }
+
+    if (!newState) {
+      newState = RichUtils.handleKeyCommand(editorState, command);
+    }
 
     if (newState) {
       onChange(newState);
@@ -46,14 +59,56 @@ export default function DraftEditor(props) {
     return 'not-handled';
   }
 
+  // This method will take all the commands from the keyboard,
+  // whatever it modifies the content or not,
+  // such as left, right, up, and down.
+  const keyBindingFn = (evt) => {
+    if (!CodeUtils.hasSelectionInBlock(contents)) return getDefaultKeyBinding(evt);
+
+    // If in code block and command is tab
+    if (evt.keyCode === 9) {
+      const newEditorState = CodeUtils.onTab(
+        evt,
+        contents,
+        2, /* maxDepth */
+      );
+      if (newEditorState !== contents) {
+        onChange(newEditorState);
+      }
+      return
+    }
+    return getDefaultKeyBinding(evt);
+  }
+
+  // Use this method to handle return key specifically.
+  // Command can either go to handleKeyCommand method or not.
+  const handleReturn = (evt) => {
+    // If not in code block, fall through to default handler behavior.
+    // Command will go to handleKeyCommand.
+    if (!CodeUtils.hasSelectionInBlock(contents)) return 'not-handled';
+
+    // If code block, use library handler behavior.
+    // Command won't go to handleKeyCommand.
+    onChange(CodeUtils.handleReturn(evt, contents));
+    return 'handled';
+  }
+
+  // Add a style specifically for code block in editor
+  const myBlockStyleFn = contentBlock => {
+    const type = contentBlock.getType();
+    if (type === 'code-block') {
+      return 'superFancyBlockquote';
+    }
+  }
+
   const onUnderlineClick = e => {
     e.preventDefault()
-    onChange(RichUtils.toggleInlineStyle(props.contents, 'UNDERLINE'));
+    onChange(RichUtils.toggleInlineStyle(contents, 'UNDERLINE'));
   }
 
   const onToggleCode = e => {
     e.preventDefault()
-    onChange(RichUtils.toggleCode(props.contents));
+    onChange(RichUtils.toggleCode(contents));
   }
 
   return (
@@ -79,14 +134,14 @@ export default function DraftEditor(props) {
 
       <Grid item>
         <Editor
-          editorState={props.contents}
+          editorState={contents}
           onChange={onChange}
           handleKeyCommand={handleKeyCommand}
+          keyBindingFn={keyBindingFn}
+          handleReturn={handleReturn}
+          blockStyleFn={myBlockStyleFn}
         />
       </Grid>
-      
     </Grid>
-      
-    
   );
 }
